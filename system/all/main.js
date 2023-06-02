78c869d7f1267a22b5cf413657a25915201f3f7c
yEAh-Games <tallinnquinton@gmail.com>
2023-06-02 19:52:17 -0400

    Improvements to system

--- system/all/main.js
+++ system/all/main.js
@@ -1,3 +1,50 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            colors: {
+                primary: { "50": "#eff6ff", "100": "#dbeafe", "200": "#bfdbfe", "300": "#93c5fd", "400": "#60a5fa", "500": "#3b82f6", "600": "#2563eb", "700": "#1d4ed8", "800": "#1e40af", "900": "#1e3a8a" }
+            }
+        },
+        fontFamily: {
+            'body': [
+                'Inter',
+                'ui-sans-serif',
+                'system-ui',
+                '-apple-system',
+                'system-ui',
+                'Segoe UI',
+                'Roboto',
+                'Helvetica Neue',
+                'Arial',
+                'Noto Sans',
+                'sans-serif',
+                'Apple Color Emoji',
+                'Segoe UI Emoji',
+                'Segoe UI Symbol',
+                'Noto Color Emoji'
+            ],
+            'sans': [
+                'Inter',
+                'ui-sans-serif',
+                'system-ui',
+                '-apple-system',
+                'system-ui',
+                'Segoe UI',
+                'Roboto',
+                'Helvetica Neue',
+                'Arial',
+                'Noto Sans',
+                'sans-serif',
+                'Apple Color Emoji',
+                'Segoe UI Emoji',
+                'Segoe UI Symbol',
+                'Noto Color Emoji'
+            ]
+        }
+    }
+}
+
+
 // SHA-256 encryption (source: https://geraintluff.github.io/sha256/)
 function sha256(ascii) {
     function rightRotate(value, amount) {
@@ -112,3 +159,49 @@ function sha256(ascii) {
 
     return result;
 }
+
+        tailwind.config = {
+            theme: {
+                extend: {
+                    colors: {
+                        primary: { "50": "#eff6ff", "100": "#dbeafe", "200": "#bfdbfe", "300": "#93c5fd", "400": "#60a5fa", "500": "#3b82f6", "600": "#2563eb", "700": "#1d4ed8", "800": "#1e40af", "900": "#1e3a8a" }
+                    }
+                },
+                fontFamily: {
+                    'body': [
+                        'Inter',
+                        'ui-sans-serif',
+                        'system-ui',
+                        '-apple-system',
+                        'system-ui',
+                        'Segoe UI',
+                        'Roboto',
+                        'Helvetica Neue',
+                        'Arial',
+                        'Noto Sans',
+                        'sans-serif',
+                        'Apple Color Emoji',
+                        'Segoe UI Emoji',
+                        'Segoe UI Symbol',
+                        'Noto Color Emoji'
+                    ],
+                    'sans': [
+                        'Inter',
+                        'ui-sans-serif',
+                        'system-ui',
+                        '-apple-system',
+                        'system-ui',
+                        'Segoe UI',
+                        'Roboto',
+                        'Helvetica Neue',
+                        'Arial',
+                        'Noto Sans',
+                        'sans-serif',
+                        'Apple Color Emoji',
+                        'Segoe UI Emoji',
+                        'Segoe UI Symbol',
+                        'Noto Color Emoji'
+                    ]
+                }
+            }
+        }
---
+++ system/login/main.js
@@ -0,0 +1,72 @@
+document.getElementById('loginForm').addEventListener('submit', function (event) {
+    event.preventDefault(); // Prevent form submission
+
+    var username = document.getElementById('username').value;
+    var password = document.getElementById('password').value;
+
+    fetch('/data/accounts.json')
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            var foundUser = data.find(function (user) {
+                return user.username === username;
+            });
+
+            if (!foundUser) {
+                alert('Account does not exist! Create it at https://accounts.yeahgames.net/create');
+            } else {
+                var encryptedPassword = sha256(password);
+
+                if (foundUser.password === encryptedPassword) {
+                    var userData = {
+                        username: foundUser.username,
+                        admin: foundUser.admin
+                    };
+
+                    writeUserData(userData);
+
+                    //  success message
+                    var successMessage = document.createElement('p');
+                    successMessage.textContent = 'Logged in!';
+                    document.body.appendChild(successMessage);
+
+                    // Redirect to param or def to homepage
+                    var continueURL = getParameterByName('continue');
+                    if (continueURL) {
+                        setTimeout(function () {
+                            window.location.href = continueURL;
+                        }, 2000); 
+                    } else {
+                        setTimeout(function () {
+                            window.location.href = 'https://www.yeahgames.net';
+                        }, 2000); 
+                    }
+                } else {
+                    alert('Incorrect password!');
+                }
+            }
+        })
+        .catch(function (error) {
+            console.error('Error:', error);
+            alert('An error occurred while logging in. Please try again later.');
+        });
+});
+
+function writeUserData(userData) {
+    var jsonData = JSON.stringify(userData);
+
+    // cookie
+    document.cookie = 'yeahgames_userdata=' + encodeURIComponent(jsonData) + '; domain=yeahgames.net; path=/';
+    localStorage.setItem('yeahgames_userdata', jsonData);
+}
+
+// param
+function getParameterByName(name) {
+    name = name.replace(/[\[\]]/g, '\\$&');
+    var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
+        results = regex.exec(window.location.href);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, ' '));
+}
