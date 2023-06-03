6e2cd5ca70e2569eaa4eaddc5d647bb7855c21bd
yEAh-Games <tallinnquinton@gmail.com>
2023-06-02 20:31:41 -0400

    Adding so that if user is logged in, they're forwarded to the URL automatically

--- system/login/main.js
+++ system/login/main.js
@@ -1,8 +1,15 @@
 document.getElementById('loginForm').addEventListener('submit', function (event) {
-    event.preventDefault(); // Prevent 
+    event.preventDefault(); // Prevent form submission
 
     var username = document.getElementById('username').value;
     var password = document.getElementById('password').value;
+
+    // Check if user is already logged in
+    var userData = getLoggedInUserData();
+    if (userData) {
+        redirectToContinueURL(userData);
+        return;
+    }
 
     fetch('/data/accounts.json')
         .then(function (response) {
@@ -25,23 +32,7 @@ document.getElementById('loginForm').addEventListener('submit', function (event)
                     };
 
                     writeUserData(userData);
-
-                    //  success message
-                    var successMessage = document.createElement('p');
-                    successMessage.textContent = 'Logged in!';
-                    document.body.appendChild(successMessage);
-
-                    // Redirect to param or def to homepage
-                    var continueURL = getParameterByName('continue');
-                    if (continueURL) {
-                        setTimeout(function () {
-                            window.location.href = continueURL;
-                        }, 2000); 
-                    } else {
-                        setTimeout(function () {
-                            window.location.href = 'https://www.yeahgames.net';
-                        }, 2000); 
-                    }
+                    redirectToContinueURL(userData);
                 } else {
                     alert('Incorrect password!');
                 }
@@ -56,12 +47,52 @@ document.getElementById('loginForm').addEventListener('submit', function (event)
 function writeUserData(userData) {
     var jsonData = JSON.stringify(userData);
 
-    // cookie
+    // Store user data in cookie and local storage
     document.cookie = 'yeahgames_userdata=' + encodeURIComponent(jsonData) + '; domain=yeahgames.net; path=/';
     localStorage.setItem('yeahgames_userdata', jsonData);
 }
 
-// param
+function getLoggedInUserData() {
+    var cookieData = getCookie('yeahgames_userdata');
+    var localStorageData = localStorage.getItem('yeahgames_userdata');
+
+    if (cookieData && localStorageData && cookieData === localStorageData) {
+        return JSON.parse(cookieData);
+    }
+
+    return null;
+}
+
+function redirectToContinueURL(userData) {
+    var continueURL = getParameterByName('continue');
+
+    if (continueURL) {
+        setTimeout(function () {
+            window.location.href = continueURL;
+        }, 2000);
+    } else {
+        setTimeout(function () {
+            window.location.href = 'https://www.yeahgames.net';
+        }, 2000);
+    }
+}
+
+// Get cookie value by name
+function getCookie(name) {
+    var cookies = document.cookie.split(';');
+
+    for (var i = 0; i < cookies.length; i++) {
+        var cookie = cookies[i].trim();
+
+        if (cookie.startsWith(name + '=')) {
+            return decodeURIComponent(cookie.substring(name.length + 1));
+        }
+    }
+
+    return null;
+}
+
+// Get URL parameter value by name
 function getParameterByName(name) {
     name = name.replace(/[\[\]]/g, '\\$&');
     var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
