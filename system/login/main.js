12d082a8eb94ab99ce631953e427a6bbb0d34d92
yEAh-Games <tallinnquinton@gmail.com>
2023-06-03 14:09:33 -0400

    Testing encasing again

--- system/login/main.js
+++ system/login/main.js
@@ -44,14 +44,69 @@ document.getElementById('loginForm').addEventListener('submit', function (event)
 });
 
 function writeUserData(userData) {
+  var usernameMapping = {
+    'a': 'bR',
+    'b': 'cY',
+    'c': 'dX',
+    'd': 'eQ',
+    'e': 'fZ',
+    'f': 'gP',
+    'g': 'hO',
+    'h': 'iN',
+    'i': 'jM',
+    'j': 'kL',
+    'k': 'lK',
+    'l': 'mJ',
+    'm': 'nI',
+    'n': 'oH',
+    'o': 'pG',
+    'p': 'qF',
+    'q': 'rE',
+    'r': 'sD',
+    's': 'tC',
+    't': 'uB',
+    'u': 'vA',
+    'v': 'wZ',
+    'w': 'xY',
+    'x': 'yX',
+    'y': 'zW',
+    'z': 'aV',
+    '0': '1Q',
+    '1': '2P',
+    '2': '3O',
+    '3': '4N',
+    '4': '5M',
+    '5': '6L',
+    '6': '7K',
+    '7': '8J',
+    '8': '9I',
+    '9': '0H',
+    '-': '-G',
+    '.': '.F',
+  };
+
+  var encasingStart = '';
+  var encasingEnd = '';
+
+  if (userData.username.length > 0) {
+    var firstChar = userData.username[0];
+    var lastChar = userData.username[userData.username.length - 1];
+
+    if (usernameMapping.hasOwnProperty(firstChar) && usernameMapping.hasOwnProperty(lastChar)) {
+      encasingStart = usernameMapping[firstChar];
+      encasingEnd = usernameMapping[lastChar].split('').reverse().join('');
+    }
+  }
+
   var jsonData = JSON.stringify(userData);
-  var encasedData = '48Ds0---' + userData.username + ',admin=' + userData.admin + '---2b9W7';
+  var encasedData = encasingStart + '---' + userData.username + ',admin=' + userData.admin + '---' + encasingEnd;
   var encryptedData = sha256(encasedData);
 
   document.cookie = 'yeahgames_userdata=' + encodeURIComponent(jsonData) + '; domain=yeahgames.net; path=/';
   document.cookie = 'yeahgames_v=' + encodeURIComponent(encryptedData) + '; domain=yeahgames.net; path=/';
   localStorage.setItem('yeahgames_userdata', jsonData);
 }
+
 
 function getLoggedInUserData() {
   var cookieData = getCookie('yeahgames_userdata');
